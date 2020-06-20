9a1211456f797c1fa7953569ea7149240e55bde1
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-06-20 12:25:31 +0530

    Create Solution-2.java

---
+++ DSA/Heaps/Sort Characters By Frequency/Solution-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    class Freq {
+        int freq;
+        char val;
+    }
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(char c: s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Freq> pq = new PriorityQueue<Freq>((Freq a, Freq b) -> {
+            if(a.freq > b.freq) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        for(char c: s.toCharArray()) {
+            if(hm.containsKey(c)) {
+                int freq = hm.get(c);
+                Freq temp = new Freq();
+                temp.freq = freq;
+                temp.val = c;
+                pq.add(temp);
+                hm.remove(c);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while(!pq.isEmpty()) {
+            Freq cur = pq.poll();
+            for(int i=0; i<cur.freq; i++) {
+                res.append(cur.val);
+            }
+        }
+        
+        return res.toString();
+    }
+}
