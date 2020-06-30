587e105ad71a8201ae769a6f21023f3edf84685b
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-06-30 22:18:52 +0530

    Lazy Brute Force

---
+++ SDE Problems/Sort 0 1 2/Solution-1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+  public static void main(String[] args) {
+    int[] arr = {0, 1, 2, 0, 2, 1};
+    Arrays.sort(arr);
+    for(int i=0; i<arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    
+ }
