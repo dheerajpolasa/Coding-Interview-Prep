efa1cc5dd80d227f528a7722263d0088891541f2
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-06-30 22:06:19 +0530

    Create Solution-3.java

---
+++ SDE Problems/Duplicate-Number/Solution-3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+  public static void main(String[] args) {
+    int[] arr = {1, 3, 4, 2, 2};
+    int duplicateNumber = findDuplicateNumber(arr);
+    System.out.println(duplicateNumber);
+  }
+  
+  private static int findDuplicateNumber(int[] arr) {
+  }
+  
+}
