1fee7f0d6c794d4511c10880111fb261dbc02bc0
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-06-30 22:51:50 +0530

    Brute Force

---
+++ SDE Problems/Kadane’s Algorithm/Solution-1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+  public static void main(String[] args) {
+    int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int maxSubArraySum = getMaxSubArraySum(arr);
+    System.out.println(maxSubArraySum);
+  }
+  
+  private static int getMaxSubArraySum(int[] arr) {
+    int sum = 0;
+    for(int i=0; i<arr.length; i++) {
+      int runningSum = 0;
+      for(int j=i; j<arr.length; j++) {
+        runningSum += arr[j];
+        sum = Math.max(sum, runningSum);
+      }
+    }
+    return sum;
+  }
+  
+}
