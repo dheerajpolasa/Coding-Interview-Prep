55db0d03159c688e1fb203ac8f3bc98c86a819c3
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-07-01 10:24:20 +0530

    Create Solution-1.java

---
+++ SDE Problems/Day-2/Pascal Triangle/Rotate Matrix/Solution-1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+  public static void main(String[] args) {
+    int[][] matrix = new int[][] {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    }
+    
+    rotateMatrix(matrix);
+    for(int i=0; i<matrix.length; i++) {
+      for(int j=0; j<matrix[i].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+  
+  private static void rotateMatrix(int[][] matrix) {
+    
+  }
+  
+}
