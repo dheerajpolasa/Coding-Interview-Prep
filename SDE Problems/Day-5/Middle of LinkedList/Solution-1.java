af09a9a1a43db738cbfebfe5c8a5998e56432763
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-07-02 10:04:19 +0530

    Create Solution-1.java

---
+++ SDE Problems/Day-5/Middle of LinkedList/Solution-1.java
@@ -0,0 +1,26 @@
+class Solution {
+  public ListNode middleNode(TreeNode head) {
+    int len = getLength(head);
+    int mid = len / 2;
+    ListNode cur = head;
+    int i = 0;
+    while(i <= mid) {
+      cur = cur.next;
+      i++;
+    }
+    
+    return cur;
+    
+  }
+  
+  private int getLength(TreeNode head) {
+    int len = 0;
+    ListNode cur = head;
+    while(cur != null) {
+      len++;
+      cur = cur.next;
+    }
+    return len;
+  }
+}
+      
