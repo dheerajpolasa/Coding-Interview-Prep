081c205e38f239010555d9a70c9169cbba8436c3
Dheeraj Polasa <34590085+dheerajpolasa@users.noreply.github.com>
2020-07-04 12:46:20 +0530

    Recursive 1

--- SDE Problems/Day-5/Reverse LinkedList/Solution-2.java
+++ SDE Problems/Day-5/Reverse LinkedList/Solution-2.java
@@ -1,7 +1,31 @@
-import java.util.*;
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-  private ListNode reverseList(ListNode head) {
+    ListNode newHead;
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode prev = reverse(head);
+        prev.next = null;
+        return newHead;
+    }
     
-  }
+    private ListNode reverse(ListNode head) {
+        if(head.next == null) {
+            newHead = head;
+            ListNode prev = head;
+            return prev;
+        }
+        ListNode prev = reverse(head.next);
+        prev.next = head;
+        prev = prev.next;
+        return prev;
+    }
 }
